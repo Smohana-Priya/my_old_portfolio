b6378b91582c3290ca5ef9d39573a95007222dbd
Smohana-Priya <smohanapriya2604@gmail.co>
2024-04-16 09:46:37 +0530

    footer design completed for both mobile and desktop

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mohana_priya_portfolio/screens/home.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Mohana Priya',
-      theme: ThemeData.dark(),
+      theme: ThemeData(textTheme: GoogleFonts.habibiTextTheme()),
       home: const Home(),
     );
   }
