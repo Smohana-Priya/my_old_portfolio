a92c5826f0dae546d2486b2bfc7cbcf8d45e55cc
Smohana-Priya <smohanapriya2604@gmail.co>
2024-04-18 14:31:30 +0530

    added url launcher package

--- lib/widgets/mobile/skills_mobile.dart
+++ lib/widgets/mobile/skills_mobile.dart
@@ -5,11 +5,25 @@ class SkillsMobile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 600,
-      color: Colors.indigoAccent,
-      child: const Text("Mobile-skills"),
+    return Wrap(
+      children: [
+        for (int i = 0; i < 4; i++)
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle, color: Colors.amber),
+              child: const Icon(
+                Icons.abc,
+                size: 45,
+              ),
+            ),
+          ),
+        const SizedBox(
+          height: 10,
+        ),
+      ],
     );
   }
 }
